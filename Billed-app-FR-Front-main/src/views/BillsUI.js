860d0f6e9e08f6f46a341779b597a563aff67533
martin.dubois <martin.dubois@foncia.com>
2023-10-19 16:01:33 +0200

    feat(newBill/test): add integration test for newBill POST

--- Billed-app-FR-Front-main/src/views/BillsUI.js
+++ Billed-app-FR-Front-main/src/views/BillsUI.js
@@ -57,7 +57,7 @@ export default ({ data: bills, loading, error }) => {
           <button type="button" data-testid='btn-new-bill' class="btn btn-primary">Nouvelle note de frais</button>
         </div>
         <div id="data-table">
-        <table id="example" class="table table-striped" style="width:100%">
+        <table id="example" class="table table-striped" style="width:100%" data-testid="table">
           <thead>
               <tr>
                 <th>Type</th>
